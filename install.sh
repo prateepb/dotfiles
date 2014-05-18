73f98d38a887a21543089929d55739f49cd604ca
Prateep Bandharangshi <prateep@waratek.com>
2014-05-18 15:49:45 +0100

    exit cleanly

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ ln -sfv $HOME/.dotfiles/.bash_profile $HOME/.bash_profile
 ln -sfv $HOME/.dotfiles/.bashrc $HOME/.bashrc
 ln -sfv $HOME/.dotfiles/.vimrc $HOME/.vimrc
 [[ -d $HOME/.vagrant.d ]] && ln -sfv $HOME/.dotfiles/vagrant.d/Vagrantfile $HOME/.vagrant.d/Vagrantfile
-
+exit 0
