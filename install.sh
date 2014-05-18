ddb641b448809f0c46ff4c28d5f41030c713c958
Prateep Bandharangshi <prateep@waratek.com>
2014-05-18 15:34:34 +0100

    Check if vagrant installed

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@
 ln -sfv $HOME/.dotfiles/.bash_profile $HOME/.bash_profile
 ln -sfv $HOME/.dotfiles/.bashrc $HOME/.bashrc
 ln -sfv $HOME/.dotfiles/.vimrc $HOME/.vimrc
-ln -sfv $HOME/.dotfiles/vagrant.d/Vagrantfile $HOME/.vagrant.d/Vagrantfile
+[[ -d $HOME/.vagrant.d ]] && ln -sfv $HOME/.dotfiles/vagrant.d/Vagrantfile $HOME/.vagrant.d/Vagrantfile
 
